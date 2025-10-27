4480c962d1ab72152e34dd022cfe9f240cd890b8
Ruba Al-Khatib <ruba.alkhatib16@gmail.com>
2025-10-27 08:48:58 +0300

    Update layout and style

--- js/script.js
+++ js/script.js
@@ -191,3 +191,18 @@ if(alisarSection){
   alisarObserver.observe(alisarSection);
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const sections = document.querySelectorAll('.fade-in-section');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.2 });
+
+  sections.forEach(section => observer.observe(section));
+});
+
